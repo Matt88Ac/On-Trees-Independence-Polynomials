034b378c6c5c25b0636edb2c3d015f2585ea148e
Matt8ac <Matt8ac@gmail.com>
2020-03-13 20:17:03 +0200

    Fixed

---
+++ ToJava/Fixed/src/Main.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+import java.lang.Math.*;
+
+@SuppressWarnings("unused")
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        TreeGraphs T = new TreeGraphs(9);
+
+    }
+
+}
---
+++ ToJava/Fixed/src/Vertex.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+class RandomInt // Generating random integer out of specific range:
+{
+    RandomInt(){};
+    public int GetRand(int min, int max) {
+        int x =  max-((int)(Math.random()*(max-min +1)));
+        return x;
+
+    }
+
+}
+
+public class Vertex {
+
+    private int index;
+    private int degree;
+    private ArrayList<Integer> nei_index;
+
+    public Vertex(int ind, int deg) { this.index = ind; this.degree=deg;  nei_index = new ArrayList<Integer>(); }
+
+    public void AddNeigh(int ind) { this.nei_index.add(ind); this.degree++; }
+    public void RemoveNeigh(int which_one) { this.nei_index.remove(which_one); this.degree--; }
+
+    public final int GetDeg() { return this.degree; }
+    public final int GetInd() { return this.index; }
+
+    public boolean isNeig(final int ind)
+    {
+        for (int i = 0; i < this.nei_index.size(); i++)  { 	if(this.nei_index.get(i) == ind) { return true; } }
+
+        return false;
+    }
+
+}
+
+
+
