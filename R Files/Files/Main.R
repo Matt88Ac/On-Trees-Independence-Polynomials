9314a8117350316b5d875fb312d733e3d9727996
Matt8ac <Matt8ac@gmail.com>
2020-03-24 18:43:25 +0200

    dels

--- R Files/Files/Main.R
+++ R Files/Files/Main.R
@@ -7,6 +7,6 @@ p$init(D)
 
 
 
-pol <-  polynomial(coef = c(5,2,4))
+pol <-  polynomial(coef = c(1,12,37,69,42,10,3))
 polyroot(pol)
 plot(pol, xlim = c(-40, 40), ylim = c(-40, 40), col = "blue", lwd = 5)
