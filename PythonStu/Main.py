bc0125e722ef5a411699a68ceb803d7341b02da1
Matt8ac <Matt8ac@gmail.com>
2020-03-27 19:04:35 +0300

    ongo

--- PythonStu/Main.py
+++ PythonStu/Main.py
@@ -1,19 +1,15 @@
 # -*- coding: utf-8 -*-
 
-import Independence_Polys as IP
-import Trees as Tr
 
-T = Tr.Tree(25)
+import Trees as Tr
+import Independence_Polys as IP
+
+T = Tr.Tree(10)
 T.print_rel()
 
+P = IP.IndeP(10)
 
-
-
-#P = IP.IndeP(10)
-
-#print(P.The_Polynom)
-#print(P.The_Tree.print_rel())
-#P.The_Polynom.show_graph(-10, 10)
-#print(P.The_Polynom.roots())
-
-
+print(P.The_Polynom)
+# print(P.The_Tree.print_rel())
+# P.The_Polynom.show_graph(-10, 10)
+# print(P.The_Polynom.roots())
--- ToJava/Fixed/src/IndependencePolynomial.java
+++ ToJava/Fixed/src/IndependencePolynomial.java
@@ -2,33 +2,8 @@ public class IndependencePolynomial {
 
     private Polynomial PolyHolder;
 
-    public IndependencePolynomial(TreeGraphs T) {  this.PolyHolder = this.ComputePoly(T); }
+    public IndependencePolynomial(TreeGraphs T) {  }
 
-    public Polynomial ComputePoly(TreeGraphs T)
-    {
-        Polynomial tmp = new Polynomial();
-
-        if(T.Get_size_V() == 0)
-        {
-            tmp.addMono(new Monomial(1,0));
-            return tmp;
-        }
-
-        if(T.isKn())
-        {
-            tmp.addMono(new Monomial(1,0));
-            tmp.addMono(new Monomial(T.Get_size_V(),1));
-
-            return tmp;
-        }
-
-
-        tmp = ComputePoly(T.SubGraph(T.Get_Max_Deg_ver(),true));
-
-        tmp.addPoly(ComputePoly(T.SubGraph(T.Get_Max_Deg_ver(),false)).mulMono_to_ret(new Monomial(1,1)));
-
-        return tmp;
-    }
 
 
 }
